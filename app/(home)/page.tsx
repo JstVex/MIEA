db2dc9783c56239bebf909ff18a48e810ac7d8e1
JstVex <heinhtetlulu14@gmail.com>
2023-11-21 11:45:23 -0800

    Create home page with Image Slider

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import ImageSlider from "@/components/home/imageSlider";
+
+const Home = () => {
+    return (
+        <div>
+            <ImageSlider />
+        </div>
+    );
+}
+
+export default Home;
---
+++ components/home/imageSlider.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import Image from 'next/image';
+import { useState } from 'react';
+import { ArrowRight, ArrowLeft } from 'lucide-react';
+
+const images = [
+    '/images/slider/img1.jpg',
+    '/images/slider/img2.jpg',
+    '/images/slider/img3.jpg',
+    '/images/slider/img4.jpg',
+];
+
+const texts = [
+    'Fast pace schedule',
+    'Top notch facilities',
+    'Fantastic results',
+    'Yearly guidance',
+];
+
+const ImageSlider = () => {
+    const [currentImage, setCurrentImage] = useState(0);
+
+    const nextImage = () => {
+        setCurrentImage((prev) => (prev + 1) % images.length);
+    };
+
+    const prevImage = () => {
+        setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
+    };
+
+    return (
+        <div className="relative w-full h-screen overflow-hidden">
+            <div className="absolute z-50 left-0 top-0 w-1/3 mt-[60vh] ml-12 bg-gray-800 text-white p-8">
+                <h1 className="ml-12 text-3xl opacity-100 font-bold uppercase text-green-500 mb-4">
+                    {texts[currentImage]}
+                </h1>
+            </div>
+            <div className="w-full relative">
+                {images.map((image, index) => (
+                    <Image
+                        key={index}
+                        src={image}
+                        width={500}
+                        height={500}
+                        alt={`Image ${index + 1}`}
+                        className={`absolute w-full h-screen object-cover transition-transform duration-300 transform ${index === currentImage ? 'translate-x-0' : 'translate-x-full'
+                            }`}
+                    />
+                ))}
+                <div className="absolute top-[80vh] right-4 flex items-center space-x-2">
+                    <button
+                        onClick={prevImage}
+                        className="w-10 h-10 bg-black border border-white text-white rounded-full flex items-center justify-center text-xl focus:outline-none"
+                    >
+                        <ArrowLeft width={20} height={20} />
+                    </button>
+                    <button
+                        onClick={nextImage}
+                        className="w-10 h-10 bg-black border border-white text-white rounded-full flex items-center justify-center text-xl focus:outline-none"
+                    >
+                        <ArrowRight width={20} height={20} />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ImageSlider;
