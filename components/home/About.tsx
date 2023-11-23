6fd1d219d8c4a9cd5fbe5445ab47981ad36aa0b8
JstVex <heinhtetlulu14@gmail.com>
2023-11-23 00:24:59 -0800

    Make the 'About' section responsive

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 const AboutSection = () => {
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 w-full h-auto mt-20">
+        <div className="grid grid-cols-1 lg:grid-cols-2 w-full h-auto mt-14 lg:mt-20">
             <div className="my-auto">
-                <div className="flex flex-col mx-10 lg:ml-10 lg:mr-0">
+                <div className="flex flex-col mx-5 lg:ml-10 lg:mr-0">
                     <div className="border-t-[6px] border-green-700 w-1/2"></div>
-                    <div className="flex flex-col mt-8 space-y-8 leading-8">
-                        <h2 className="text-4xl text-slate-700 font-bold">
+                    <div className="flex flex-col mt-8 space-y-4 md:space-y-8 leading-8">
+                        <h2 className="text-3xl md:text-4xl text-slate-700 font-black">
                             At MIEA, we know, value and challenge each student
                         </h2>
                         <p className="font-light">
@@ -27,17 +27,17 @@ const AboutSection = () => {
                     </div>
                 </div>
             </div>
-            <div className="mx-auto  mt-10 lg:mt-0">
+            <div className="mx-auto mt-10 lg:mt-0">
                 <div className="relative overflow-hidden">
                     <Image
                         src='https://images.unsplash.com/photo-1700405936040-a593ec18ec78?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2NXx8fGVufDB8fHx8fA%3D%3D'
                         alt="hey"
                         width={500}
                         height={500}
-                        className="w-[85vw] h-[60vw] lg:w-96 lg:h-[70vh]"
+                        className="w-[85vw] h-[60vw] lg:w-96 lg:h-[70vh] xl:w-[34rem] xl:h-[85vh]"
                     />
                 </div>
-                <div className="absolute bg-green-700 right-[55%] -bottom-[120%] lg:right-[21%] lg:-bottom-[56%] w-[85vw] h-[60vw] lg:w-96 lg:h-[70vh] transform translate-x-1/2 translate-y-1/2 -z-10"></div>
+                <div className="absolute bg-green-700 right-[55%] min-[200px]:-bottom-[145%] min-[300px]:-bottom-[130%] min-[450px]:-bottom-[115%] sm:-bottom-[110%] lg:right-[21%] lg:-bottom-[53%] lg:w-96 lg:h-[70vh] xl:right-[21%] xl:-bottom-[60%] w-[85vw] h-[60vw] xl:w-[34rem] xl:h-[85vh] transform translate-x-1/2 translate-y-1/2 -z-10"></div>
             </div>
 
         </div>
