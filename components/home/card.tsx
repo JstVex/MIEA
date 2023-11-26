802becf832351769bc216e9d2ba52475d3aaa4bf
JstVex <heinhtetlulu14@gmail.com>
2023-11-25 21:33:44 -0800

    Add bounce effect on hover

--- components/home/card.tsx
+++ components/home/card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, description }) => {
     return (
-        <div className="relative mx-4 mt-1 w-[22rem] h-[15rem]">
+        <div className="relative mx-4 mt-1 w-[22rem] h-[15rem] hover:-translate-y-2 transition-all">
             <div className="border-2 bg-white border-green-700 rounded-xl p-4 relative z-10">
                 <h2 className="font-semibold text-lg">
                     {title}
